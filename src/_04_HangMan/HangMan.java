2372ab0385d0681d7c0411cc85218cc2413b283f
bobram22 <league@Mac-mini-3.local>
2018-09-05 18:27:10 -0700

    Worked on Hangman

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -1,7 +1,16 @@
 package _04_HangMan;
 
+import java.util.Stack;
+
+import javax.swing.JOptionPane;
+
 public class HangMan{
-	
-	
+		static Stack<String> s = new Stack<String>();
+	public static void main(String[] args) {
+		String sNum = JOptionPane.showInputDialog("How many words would you like to guess?");
+		int num = Integer.parseInt(sNum);
+		Utilities.readRandomLineFromFile("dictionary.txt");
+		
+	}
 	
 }
